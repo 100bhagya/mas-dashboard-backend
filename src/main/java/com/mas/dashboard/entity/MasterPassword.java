8317d461b6ec993cedb6be18ca0101ae16311778
jaditya8109 <jaditya8109@gmail.com>
2022-11-22 22:18:44 +0530

    expiration of maste pswd added

--- src/main/java/com/mas/dashboard/entity/MasterPassword.java
+++ src/main/java/com/mas/dashboard/entity/MasterPassword.java
@@ -1,15 +1,17 @@
 package com.mas.dashboard.entity;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableScheduling
 public class MasterPassword {
     private String masterPassword = null;
     public String getMasterPassword() { return masterPassword; }
     public void setMasterPassword(String masterPassword) {this.masterPassword = masterPassword;}
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelay = 24 * 60 * 60 * 1000)
     public void setMasterPasswordToNull(){
         this.masterPassword = null;
     }
