df47f8ad87fd4bf37bc6ab67e09d2428610331ba
Harikesh Singh <91447902+Harikesh04@users.noreply.github.com>
2023-02-17 00:22:21 +0530

    Added  API for Uploading data from google sheets to DB. (#29)
    * added studentsData Api for Google sheets
    
    * added liquibase file for students data

---
+++ src/main/java/com/mas/dashboard/Helper/StudentsDataHelper.java
@@ -0,0 +1,109 @@
+package com.mas.dashboard.Helper;
+
+import com.mas.dashboard.entity.StudentData;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentsDataHelper {
+
+    public static boolean checkExcelFormat(MultipartFile file) {
+
+        String contentType = file.getContentType();
+        //checking that file is of Excel type or not
+
+       return  (contentType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+
+
+    }
+
+    public  static List<StudentData> convertExcelToListOfStudentsData(InputStream is){
+        List<StudentData> list = new ArrayList<>();
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook(is);
+
+            XSSFSheet sheet = workbook.getSheet("MasterData");
+
+            int rowNumber = 0;
+            Iterator<Row> iterator = sheet.iterator();
+
+            while(iterator.hasNext()){
+                Row row = iterator.next();
+
+                if (rowNumber == 0) {
+                    rowNumber++;
+                    continue;
+                }
+                Iterator<Cell> cells = row.iterator();
+                int cellId =0;
+                StudentData data = new StudentData();
+
+                while (cells.hasNext()){
+                    Cell cell = cells.next();
+                    switch (cellId){
+                        case 0:
+                            data.setId((long) cell.getNumericCellValue()) ;
+                            break;
+                        case 1:
+                            data.setRollNumber( cell.getStringCellValue());
+                            break;
+                        case 2:
+                            data.setStudentName(cell.getStringCellValue());
+                            break;
+                        case 3:
+                            data.setTotalMarks((int) cell.getNumericCellValue() );
+                            break;
+                        case 4:
+                            data.setRank((int) cell.getNumericCellValue());
+                            break;
+                        case 5:
+                            data.setPercentile( cell.getNumericCellValue());
+                            break;
+                        case 6:
+                            data.setTestDate(cell.getDateCellValue());
+                            break;
+                        case 7:
+                            data.setTestName( cell.getStringCellValue());
+                            break;
+                        case 8:
+                            data.setHelper(cell.getStringCellValue());
+                            break;
+                        case 9:
+                            data.setRecentTestName( cell.getStringCellValue());
+                            break;
+                        case 10:
+                            data.setPercentileFinal(cell.getNumericCellValue());
+                            break;
+                        case 11:
+                            data.setMarksFinal((int) cell.getNumericCellValue());
+                            break;
+                        default:
+                            break;
+
+
+                    }
+
+                    cellId++;
+
+
+                }
+                list.add(data);
+
+
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/mas/dashboard/entity/StudentData.java
@@ -0,0 +1,142 @@
+package com.mas.dashboard.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "students_data")
+public class StudentData {
+    @Id
+    private Long id;
+
+    private String rollNumber;
+
+    private  String studentName;
+
+    private  Integer totalMarks;
+    private  Integer rank;
+    private  Double percentile;
+
+    private Date testDate;
+
+    private String testName;
+    private String helper;
+    private String recentTestName;
+
+    private Double percentileFinal;
+    private Integer marksFinal;
+
+    public StudentData(Long id, String rollNumber, String studentName, Integer totalMarks, Integer rank, Double percentile, Date testDate, String testName, String helper, String recentTestName, Double percentileFinal, Integer marksFinal) {
+        this.id = id;
+        this.rollNumber = rollNumber;
+        this.studentName = studentName;
+        this.totalMarks = totalMarks;
+        this.rank = rank;
+        this.percentile = percentile;
+        this.testDate = testDate;
+        this.testName = testName;
+        this.helper = helper;
+        this.recentTestName = recentTestName;
+        this.percentileFinal = percentileFinal;
+        this.marksFinal = marksFinal;
+    }
+
+    public StudentData() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Integer getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(Integer totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public Double getPercentile() {
+        return percentile;
+    }
+
+    public void setPercentile(Double percentile) {
+        this.percentile = percentile;
+    }
+
+    public Date getTestDate() {
+        return testDate;
+    }
+
+    public void setTestDate(Date testDate) {
+        this.testDate = testDate;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getHelper() {
+        return helper;
+    }
+
+    public void setHelper(String helper) {
+        this.helper = helper;
+    }
+
+    public String getRecentTestName() {
+        return recentTestName;
+    }
+
+    public void setRecentTestName(String recentTestName) {
+        this.recentTestName = recentTestName;
+    }
+
+    public Double getPercentileFinal() {
+        return percentileFinal;
+    }
+
+    public void setPercentileFinal(Double percentileFinal) {
+        this.percentileFinal = percentileFinal;
+    }
+
+    public Integer getMarksFinal() {
+        return marksFinal;
+    }
+
+    public void setMarksFinal(Integer marksFinal) {
+        this.marksFinal = marksFinal;
+    }
+}
