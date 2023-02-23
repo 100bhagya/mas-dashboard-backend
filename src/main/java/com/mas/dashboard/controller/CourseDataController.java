d4a5cb1982d1a39af5de32f3417957d2f635b33f
Harry <harikeshkrsingh20@gmail.com>
2023-02-23 19:41:34 +0530

    added API for course data

---
+++ src/main/java/com/mas/dashboard/controller/CourseDataController.java
@@ -0,0 +1,65 @@
+package com.mas.dashboard.controller;
+
+import com.mas.dashboard.Helper.CourseDataHelper;
+
+import com.mas.dashboard.entity.AppUser;
+import com.mas.dashboard.entity.CourseData;
+
+import com.mas.dashboard.payload.response.MessageResponse;
+import com.mas.dashboard.repository.AppUserRepository;
+import com.mas.dashboard.security.services.AppUserDetailsImpl;
+import com.mas.dashboard.service.CourseDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/course")
+public class CourseDataController {
+
+    @Autowired
+    private CourseDataService courseDataService;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @PostMapping("/data/upload")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) {
+        if(CourseDataHelper.checkExcelFormat(file)){
+            this.courseDataService.save(file);
+
+            return ResponseEntity.ok(new MessageResponse("The file is Uploaded and data is saved in the db successfully !"));
+
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please upload excel file ");
+
+    }
+
+    @GetMapping("/data")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<List<CourseData>> getAllStudentData() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        AppUserDetailsImpl loggedInUser = (AppUserDetailsImpl)auth.getPrincipal();
+
+        final Optional<AppUser> optionalAppUser = this.appUserRepository.findByEmail(loggedInUser.getEmail());
+        if(!optionalAppUser.isPresent()){
+            throw new UsernameNotFoundException("No user found");
+        }
+
+        final AppUser user =  optionalAppUser.get();
+        return ResponseEntity.ok(this.courseDataService.getAllCourseData(user.getRollNo()));
+    }
+
+}
+
---
+++ src/main/java/com/mas/dashboard/entity/CourseData.java
@@ -0,0 +1,106 @@
+package com.mas.dashboard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "course_data")
+public class CourseData {
+    @Id
+    private Long id;
+
+    private String courseName;
+
+    private  String studentName;
+
+    @Email
+    private  String Email;
+
+    private  Integer timeSpentMins;
+
+    private Double timeSpentHours;
+
+    private Integer progress;
+
+    private String rollNumber;
+
+    public CourseData(Long id, String courseName, String studentName, String email, Integer timeSpentMins, Double timeSpentHours, Integer progress, String rollNumber) {
+        this.id = id;
+        this.courseName = courseName;
+        this.studentName = studentName;
+        Email = email;
+        this.timeSpentMins = timeSpentMins;
+        this.timeSpentHours = timeSpentHours;
+        this.progress = progress;
+        this.rollNumber = rollNumber;
+    }
+
+    public CourseData() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Integer getTimeSpentMins() {
+        return timeSpentMins;
+    }
+
+    public void setTimeSpentMins(Integer timeSpentMins) {
+        this.timeSpentMins = timeSpentMins;
+    }
+
+    public Double getTimeSpentHours() {
+        return timeSpentHours;
+    }
+
+    public void setTimeSpentHours(Double timeSpentHours) {
+        this.timeSpentHours = timeSpentHours;
+    }
+
+    public Integer getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Integer progress) {
+        this.progress = progress;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+}
